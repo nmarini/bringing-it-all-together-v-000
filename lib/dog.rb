585fe9f661fb7961901b99729d4c94f061eebcb7
Nicolas Marini <nmarini44@gmail.com>
2018-08-29 21:56:59 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -30,10 +30,10 @@ attr_reader :id
   end
 
   def self.find_by_name(name)
-    sql = <<-SQL
-      SELECT * FROM dogs
-      WHERE name = ?
-    SQL
+    # sql = <<-SQL
+    #   SELECT * FROM dogs
+    #   WHERE name = ?
+    # SQL
 
     # results = DB[:conn].execute(sql, name)
 binding.pry
